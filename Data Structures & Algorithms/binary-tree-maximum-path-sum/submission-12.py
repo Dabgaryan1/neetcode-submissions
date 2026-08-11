d3c4eb551e5a56b64ab53a0909bfea1600c01aea
Daniel Abgaryan <152477317+Dabgaryan1@users.noreply.github.com>
2026-08-10 23:43:51 -0700

    Add: binary-tree-maximum-path-sum - submission-12

---
+++ Data Structures & Algorithms/binary-tree-maximum-path-sum/submission-12.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        self.best = root.val
+
+        def dfs(node):
+            if not node:
+                return 0
+            left = dfs(node.left)
+            right = dfs(node.right)
+            self.best = max(self.best, node.val + left + right)
+            return max(node.val + left, node.val + right, 0)
+        
+        dfs(root)
+        return self.best
